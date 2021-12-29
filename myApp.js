396200eeb1e9bb585e2479af8abac11e8b657ca3
jinjio <35422448+jinjio@users.noreply.github.com>
2021-12-29 19:30:00 +0900

    .

--- myApp.js
+++ myApp.js
@@ -1,39 +1,17 @@
 var express = require('express');
 var app = express();
+let absolutePath = __dirname + "/views/index.html"
+// require('dotenv').config()
+ 
+app.use(express.static(__dirname + "/public"));
 
+//console.log("Hello World")
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/json", (req, res) => {
+  res.json({"message": process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON": "Hello json"})
+})
  module.exports = app;
+
